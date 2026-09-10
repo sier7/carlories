e59d7d173ca729ef44fbbbffd3a1e7e181529979
sier7 <1270568092@qq.com>
2026-09-10 23:29:07 +0800

    更新 storage/dayRepo.js

--- storage/dayRepo.js
+++ storage/dayRepo.js
@@ -19,7 +19,7 @@ const STORE = 'dayHealth'
 
 export const SOURCE_MANUAL = 'manual'
 export const SOURCE_SHORTCUT = 'shortcut'
-export const SOURCE_RELAY = 'relay'
+export const SOURCE_MAILBOX = 'mailbox'
 
 export async function getDayHealth(date) {
   return (await get(STORE, date)) || null
